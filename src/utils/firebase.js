cf14c136f8a736f7b33b7e8dd431e4f9366c445c
jkd65 <jkd65@nau.edu>
2018-11-11 13:50:42 -0600

    firebase points at enviornment variables now

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,7 +3,14 @@ import 'firebase/database'
 import 'firebase/auth'
 import {development} from './keys'
 
-firebase.initializeApp(development)
+firebase.initializeApp({
+  apiKey: development.apiKey || process.env.API_KEY,
+  authDomain: development.authDomain || process.env.AUTH_DOMAIN,
+  databaseURL: development.databaseURL || process.env.DATABASE_URL,
+  projectId: development.projectId || process.env.PROJECT_ID,
+  storageBucket: development.storageBucket || process.env.STORAGE_BUCKET,
+  messagingSenderId: development.messagingSenderId || process.env.MESSAGING_SENDER_ID,
+})
 const database = firebase.database()
 
 export {firebase, database}
