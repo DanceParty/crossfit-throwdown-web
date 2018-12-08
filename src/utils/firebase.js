ad85a4b0261d61d81300a8f1be7207de01fc817a
jkd65 <jkd65@nau.edu>
2018-12-07 20:31:35 -0600

    test

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,30 +2,25 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 import 'firebase/auth'
 
-console.log(process.env)
-if (process.env.NODE_ENV === 'development' || 'test') {
-  console.log('sinde development')
-  firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY_DEV,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN_DEV,
-    databaseURL: process.env.REACT_APP_DATABASE_URL_DEV,
-    projectId: process.env.REACT_APP_PROJECT_ID_DEV,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET_DEV,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_DEV,
-  })
-} else {
-  console.log(process.env)
-  console.log('inside else')
-  firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  })
-  console.log(firebase)
-}
+// if (process.env.NODE_ENV === 'development' || 'test') {
+//   firebase.initializeApp({
+//     apiKey: process.env.REACT_APP_API_KEY_DEV,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN_DEV,
+//     databaseURL: process.env.REACT_APP_DATABASE_URL_DEV,
+//     projectId: process.env.REACT_APP_PROJECT_ID_DEV,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET_DEV,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID_DEV,
+//   })
+// }
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+})
 const database = firebase.database()
 
 export {firebase, database}
